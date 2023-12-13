40e58df6c4e28080f8ed7c3585030ce14f598c4d
lukesnc <lukeasimone@gmail.com>
2023-12-13 13:29:07 -0500

    day 8 done

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -29,33 +29,63 @@ fn parse() -> (String, BTreeMap<String, (String, String)>) {
     (instructions, network)
 }
 
+fn gcd(mut a: u64, mut b: u64) -> u64 {
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    if a == 0 || b == 0 {
+        0
+    } else {
+        (a * b) / gcd(a, b)
+    }
+}
+
+fn lcm_of_vec(numbers: &Vec<u64>) -> u64 {
+    if numbers.is_empty() {
+        0
+    } else {
+        let mut result = numbers[0];
+        for &number in numbers.iter().skip(1) {
+            result = lcm(result, number);
+        }
+        result
+    }
+}
+
 fn main() {
     let (instructions, network) = parse();
-    let mut instr_loop = instructions.chars().cycle();
 
-    let mut paths: Vec<String> = network
+    let mut paths: Vec<(String, u64)> = network
         .keys()
         .cloned()
         .into_iter()
         .filter(|k| k.ends_with('A'))
+        .map(|x| (x, 0))
         .collect();
 
-    let mut steps: u64 = 0;
-    loop {
-        // print!("{:?}\r", &paths);
-
-        let direction = instr_loop.next().unwrap();
-        for path in &mut paths {
+    for (path, steps) in &mut paths {
+        let mut instr_loop = instructions.chars().cycle();
+        loop {
+            let direction = instr_loop.next().unwrap();
             *path = match direction {
                 'L' => network.get(path).unwrap().0.to_owned(),
                 _ => network.get(path).unwrap().1.to_owned(),
             };
-        }
-        steps += 1;
+            *steps += 1;
 
-        if paths.iter().all(|v| v.ends_with('Z')) {
-            break;
+            if path.ends_with('Z') {
+                break;
+            }
         }
     }
-    println!("\n\n{}", steps);
+
+    let lengths: Vec<u64> = paths.iter().map(|x| x.1).collect();
+    let res = lcm_of_vec(&lengths);
+    println!("{}", res);
 }
