66dc2c544d8d7d913ad7e8f858965fc037afc12f
lukesnc <lukeasimone@gmail.com>
2023-12-13 04:11:58 -0500

    removing AoC input/examples, sorry :(

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -33,18 +33,29 @@ fn main() {
     let (instructions, network) = parse();
     let mut instr_loop = instructions.chars().cycle();
 
-    let mut steps = 0;
-    let mut cur = network.keys().nth(0).unwrap();
+    let mut paths: Vec<String> = network
+        .keys()
+        .cloned()
+        .into_iter()
+        .filter(|k| k.ends_with('A'))
+        .collect();
+
+    let mut steps: u64 = 0;
     loop {
-        cur = match instr_loop.next().unwrap() {
-            'L' => &network.get(cur).unwrap().0,
-            _ => &network.get(cur).unwrap().1,
-        };
+        // print!("{:?}\r", &paths);
+
+        let direction = instr_loop.next().unwrap();
+        for path in &mut paths {
+            *path = match direction {
+                'L' => network.get(path).unwrap().0.to_owned(),
+                _ => network.get(path).unwrap().1.to_owned(),
+            };
+        }
         steps += 1;
 
-        if cur == "ZZZ" {
+        if paths.iter().all(|v| v.ends_with('Z')) {
             break;
         }
     }
-    println!("{}", steps);
+    println!("\n\n{}", steps);
 }
